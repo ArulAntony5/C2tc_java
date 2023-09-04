cddf5996c8df33b46af7aa05d95e8abe5bae8f6a
Arul Antony <Arul Antony@LAPTOP-O5DIEMGU>
2023-09-04 10:48:24 +0530

    first commit

---
+++ c2tc_javapg/src/com/tns/inheritance/SingleinheritanceDemo.java
@@ -0,0 +1,12 @@
+package com.tns.inheritance;
+
+public class SingleinheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student st=new Student("arul","1234567890","chennai","0987654321","mcc");
+				System.out.println(st);
+	}
+
+}
---
+++ c2tc_javapg/src/com/tns/staticprog/StaticDemo.java
@@ -0,0 +1,14 @@
+package com.tns.staticprog;
+
+public class StaticDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+		System.out.println(Employee.companyName);
+		Employee e=new Employee("Ramya",123);
+		System.out.println(e);
+		
+	}
+
+}
